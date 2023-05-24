f1d311d30d96662005dcb89b2098a76721ee05da
Ma Seo Sen <pyhteam.solutions@hotmail.com>
2023-05-24 10:15:24 +0700

    dfsd

--- HMZ.WebApp/Areas/Administrator/Controllers/RolesController.cs
+++ HMZ.WebApp/Areas/Administrator/Controllers/RolesController.cs
@@ -23,32 +23,32 @@ namespace HMZ.WebApp.Areas.Administrator.Controllers
         {
             query.PageNumber = query.PageNumber > 0 ? query.PageNumber : 1;
             query.PageSize = query.PageSize > 0 ? query.PageSize : 10;
-            var users = await _service.GetPageList(query);
-            return users.Items == null ? BadRequest(users) : Ok(users);
+            var roles = await _service.GetPageList(query);
+            return  Ok(roles);
         }
         [HttpPost]
         public async Task<IActionResult> Create(RoleQuery query)
         {
             var result = await _service.CreateAsync(query);
-            return result.Entity ? Ok(result) : BadRequest(result);
+            return Ok(result);
         }
         [HttpPost]
         public async Task<IActionResult> Delete(string id)
         {
             var result = await _service.DeleteAsync(id);
-            return result.Entity > 0 ? Ok(result) : BadRequest(result);
+            return Ok(result);
         }
         [HttpPost]
         public async Task<IActionResult> Update(RoleQuery query)
         {
             var result = await _service.UpdateAsync(query, query.Name);
-            return result.Entity >0 ? Ok(result) : BadRequest(result);
+            return Ok(result);
         }
         [HttpPost]
         public async Task<IActionResult> GetById(string roleName)
         {
             var result = await _service.GetByIdAsync(roleName);
-            return result.Entity == null ?  BadRequest(result) : Ok(result);
+            return Ok(result);
         }
 
     }
