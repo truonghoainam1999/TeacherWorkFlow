0c3201d97c6d19c819c87eda669d8c32ad4261c5
Ma Seo Sen <pyhteam.solutions@hotmail.com>
2023-05-24 12:15:15 +0700

    fix

---
+++ HMZ.Database/Migrations/20230524051119_ondeleteCasecade.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HMZ.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ondeleteCasecade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_ClassRooms_RoomId",
+                table: "Schedules");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_ClassRooms_RoomId",
+                table: "Schedules",
+                column: "RoomId",
+                principalTable: "ClassRooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_ClassRooms_RoomId",
+                table: "Schedules");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_ClassRooms_RoomId",
+                table: "Schedules",
+                column: "RoomId",
+                principalTable: "ClassRooms",
+                principalColumn: "Id");
+        }
+    }
+}
--- HMZ.WebApp/Areas/Administrator/Controllers/ClassRoomController.cs
+++ HMZ.WebApp/Areas/Administrator/Controllers/ClassRoomController.cs
@@ -1,4 +1,5 @@
 ﻿using HMZ.DTOs.Fillters;
+using HMZ.DTOs.Queries.Base;
 using HMZ.DTOs.Queries.Catalog;
 using HMZ.DTOs.Views;
 using HMZ.Service.Services.ClassRoomService;
@@ -7,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HMZ.WebApp.Areas.Administrator.Controllers
 {
-    public class ClassRoomController : CRUDBaseControlle<IClassRoomService, ClassRoomQuery, ClassRoomView, ClassRoomFilter>
+    public class ClassRoomController : BaseController<IClassRoomService>
     {
         public ClassRoomController(IClassRoomService service) : base(service)
         {
@@ -27,5 +28,41 @@ namespace HMZ.WebApp.Areas.Administrator.Controllers
 
             return View();
         }
+
+        #region  CRUD
+        [HttpPost]
+        public async Task<IActionResult> GetAll(BaseQuery<ClassRoomFilter> query)
+        {
+            query.PageNumber = query.PageNumber > 0 ? query.PageNumber : 1;
+            query.PageSize = query.PageSize > 0 ? query.PageSize : 10;
+            var classRooms = await _service.GetPageList(query);
+            return Ok(classRooms);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ClassRoomQuery query)
+        {
+            var result = await _service.CreateAsync(query);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromBody] string id)
+        {
+            var result = await _service.DeleteAsync(id);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update([FromBody] ClassRoomQuery query, string id)
+        {
+            var result = await _service.UpdateAsync(query, id);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        #endregion
     }
 }
